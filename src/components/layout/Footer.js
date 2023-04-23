bd54100cb3ab05e7bc88fb04ac18b379db72fa7f
luizantonioprg <43731038+luizantonioprg@users.noreply.github.com>
2023-04-23 15:54:35 -0300

    initial commit
    initial commit

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,26 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.socialList}>
+                <li>
+                    <FaFacebook/>
+                </li>
+                <li>
+                    <FaInstagram/>
+                </li>
+                <li>
+                    <FaLinkedin/>
+                </li>
+            </ul>
+            <p className={styles.copyRight}>
+                <span className={styles.span}>Costs</span> &copy; 2021
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
